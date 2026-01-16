af1e9ba7508035819cb39eb374deb40fa983645d
Mario Esteban Espitia Martinez <71477601+marckes04@users.noreply.github.com>
2026-01-16 19:49:35 +0100

    Vercel Comfigured

--- Client/src/components/pages/Inicio.jsx
+++ Client/src/components/pages/Inicio.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 export const Inicio = () => {
   return (
     <div className="jumbo">
-      <h1>Bienvenido al blog con Rect</h1>
+      <h1>Bienvenido al blog con React</h1>
       <p>Desarrollo MERN(mongo DB, Express, React, Node)</p>
       <Link to="/articulos" className='button'> Ver los Articulos</Link>
     </div>
